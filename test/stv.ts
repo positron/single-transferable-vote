09a70b66a83aeb5b082ef337a35e67028cbeb2f4
Philip Jagielski <philipjagielski@gmail.com>
2016-08-01 00:12:10 -0500

    Add more tests (some failing)

--- test/stv.ts
+++ test/stv.ts
@@ -45,6 +45,51 @@ class STV {
     // TODO: what should happen for [vote1, vote2] and [vote12, vote2]?
   }
 
+  // Simple scenarios where one candidate wins the majority of first preference votes
+  @test oneWinnerMultipleCandidatesOneRound() {
+    const candidates3 = [1, 2, 3];
+    const vote1: Ballot = { orderedPreferences: [1] };
+    const vote123: Ballot = { orderedPreferences: [1, 2, 3] };
+    const vote32: Ballot = { orderedPreferences: [3, 2] };
+
+    expect(getWinners(candidates3, [vote1], 1)).to.deep.equal([1]);
+    expect(getWinners(candidates3, [vote123], 1)).to.deep.equal([1]);
+    expect(getWinners(candidates3, [vote123, vote1], 1)).to.deep.equal([1]);
+    expect(getWinners(candidates3, [vote1, vote123, vote32], 1)).to.deep.equal([1]);
+    expect(getWinners(candidates3, [vote32, vote123, vote1], 1)).to.deep.equal([1]);
+    expect(getWinners(candidates3, [vote32, vote123, vote32], 1)).to.deep.equal([3]);
+
+    const candidates10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const vote10: Ballot = { orderedPreferences: [10] };
+    const vote12345678910: Ballot = { orderedPreferences: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] };
+    const vote10987654321: Ballot = { orderedPreferences: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1] };
+    const vote8642: Ballot = { orderedPreferences: [8, 6, 4, 2] };
+    const vote3579: Ballot = { orderedPreferences: [3, 5, 7, 9] };
+
+    expect(getWinners(candidates10, [vote1], 1)).to.deep.equal([1]);
+    expect(getWinners(candidates10, [vote12345678910], 1)).to.deep.equal([1]);
+    expect(getWinners(candidates10, [vote12345678910, vote1], 1)).to.deep.equal([1]);
+    expect(getWinners(candidates10, [vote12345678910, vote1, vote10987654321, vote32, vote123], 1)).to.deep.equal([1]);
+    expect(getWinners(candidates10, [vote8642, vote32, vote3579, vote10987654321, vote1, vote8642], 1)).to.deep.equal([8]);
+    expect(getWinners(candidates10, [vote12345678910, vote10987654321, vote10, vote32, vote8642], 1)).to.deep.equal([10]);
+  }
+
+  // Scenarios where the vote has to go multiple rounds due to candidates not getting to 51% on the first round
+  @test oneWinnerMultipleCandidatesLoserElimination() {
+    const candidates3 = [1, 2, 3];
+    const vote1: Ballot = { orderedPreferences: [1] };
+    const vote2: Ballot = { orderedPreferences: [2] };
+    const vote123: Ballot = { orderedPreferences: [1, 2, 3] };
+    const vote32: Ballot = { orderedPreferences: [3, 2] };
+
+    expect(getWinners(candidates3, [vote1, vote32, vote2], 1)).to.deep.equal([2]);
+  }
+
+  // Scenarios where the votes only have to go one round to determine multiple winners
+  @test multipleWinnersOneRound() {
+    // TODO
+  }
+
   // TOOD: what happens when you are spreading votes but nobody (or very few)
-  //       ballots have a second choice after that candidate?
+  //       ballots have a second choice after that candidate? (What is the term openSTV uses for that?)
 }
